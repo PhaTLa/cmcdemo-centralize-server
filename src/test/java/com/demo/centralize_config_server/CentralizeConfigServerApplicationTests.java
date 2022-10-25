4e885c33ed2d8611a279c706cd346f822063a319
PhaTLa <facelessinhuman@gmail.com>
2022-10-25 16:46:33 +0700

    initial commit

---
+++ src/test/java/com/demo/centralize_config_server/CentralizeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.centralize_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
